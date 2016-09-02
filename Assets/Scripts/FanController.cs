6a1c8859b9c38bf87b35e2e7f4867e80b92f9100
Aleksander Małkowicz <olek994@wp.pl>
2016-09-02 21:37:54 +0200

    FanMovingAround
    firstMovingAround

--- Assets/Scripts/FanController.cs
+++ Assets/Scripts/FanController.cs
@@ -5,6 +5,8 @@ using System.Collections;
 public class FanController : MonoBehaviour {
 
     public GameObject player;
+    public float rotationSpeed = 0f;
+    public float radius = 0f;
     Transform fanTransform;
     
     // Use this for initialization
@@ -26,11 +28,26 @@ public class FanController : MonoBehaviour {
 
     void movingToPlayer()
     {
-        if (Vector2.Distance(fanTransform.position, player.transform.position) >= 2)
+        if (Vector2.Distance(fanTransform.position, player.transform.position) >= radius)
         { 
-            Vector2 moving = Vector2.MoveTowards(fanTransform.position, player.transform.position, 3 * Time.deltaTime);
+            Vector2 moving = Vector2.MoveTowards(fanTransform.position, player.transform.position, player.GetComponent<PlayerBehaviour>().speed * Time.deltaTime);
             fanTransform.position = moving;
         }
         
+            RotatingAroundPlayer();
+        
+        //else
+        //{
+        //    Vector2 moving = Vector2.MoveTowards(fanTransform.position, player.transform.position, -1*player.GetComponent<PlayerBehaviour>().speed * Time.deltaTime);
+        //    fanTransform.position = moving;
+        //}
+        
+    }
+
+    void RotatingAroundPlayer()
+    {
+        Quaternion q = transform.rotation;
+        transform.RotateAround(player.transform.position, Vector3.forward /2, rotationSpeed * Time.deltaTime);
+        transform.rotation = q;
     }
 }
