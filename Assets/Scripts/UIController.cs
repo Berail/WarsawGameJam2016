0501da7796c52772b2674372f9b479cce986b73c
Berail <berail@windowslive.com>
2016-09-03 12:26:44 +0200

    Adding effects and Fan and UI
    Flares and Smoke
    Fan sprite and animation
    UI to count Fans

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+
+    public Text fanCountText;
+    private GameObject Player;
+	// Use this for initialization
+	void Start () {
+        Player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        fanCountText.text = Player.GetComponent<PlayerBehaviour>().fanCount.ToString();
+	}
+}
