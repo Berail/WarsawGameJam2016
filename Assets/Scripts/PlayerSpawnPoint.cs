2f93042e02118f21ea00b88c6f57eed4b966bfe5
Berail <berail@windowslive.com>
2016-09-03 04:10:08 +0200

    SpawnPoint script and prefab

---
+++ Assets/Scripts/PlayerSpawnPoint.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSpawnPoint : MonoBehaviour {
+
+    GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+        Player = GameObject.Find("Player");
+        Player.transform.position = transform.position;
+	}
+}
